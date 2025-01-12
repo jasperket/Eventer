1fd40e17ca6899c268ebbfc3d5622b6126f9f884
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-01-12 20:27:33 +0800

    Added link to homepage

--- includes/nav-guest.php
+++ includes/nav-guest.php
@@ -3,7 +3,9 @@
 <div class="h-screen flex flex-col">
     <div class="pt-8">
         <header class="flex items-center justify-between">
-            <h2 class="text-4xl font-bold text-red-500">Eventer</h2>
+            <a href="/" class="hover:opacity-80">
+                <h2 class="text-4xl font-bold text-red-500">Eventer</h2>
+            </a>
             <div class="flex gap-2">
                 <a href="login.php" class="bg-red-600 text-white font-bold px-4 py-2 rounded hover:bg-red-500">Log in</a>
                 <a href="register.php" class="px-4 py-2 rounded hover:underline">Sign up</a>
