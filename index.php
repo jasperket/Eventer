a894b16d44daf1334849ba4c9918cefb66a0cbf8
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-01-12 16:51:29 +0800

    Added repetitive divs to nav-guest

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php require_once 'includes/header.php' ?>
 
-<div class="h-screen flex flex-col">
-    <div class="pt-8">
-        <?php require_once 'includes/nav-guest.php' ?>
-    </div>
+<?php require_once 'includes/nav-guest.php' ?>
 
-    <main class="text-center h-full flex-1 flex flex-col gap-8 items-center">
-        <h2 class="text-7xl font-bold mt-72">Your events,<br> <span class="text-red-600">expertly managed</span></h2>
-        <p class="w-[75ch]">Take control of your event lifecycle with seamless registration, real-time tracking, and intelligent capacity management. Focus on creating great experiences while we handle the details.</p>
-    </main>
+<main class="text-center h-full flex-1 flex flex-col gap-8 items-center">
+    <h2 class="text-7xl font-bold mt-72">Your events,<br> <span class="text-red-600">expertly managed</span></h2>
+    <p class="w-[75ch]">Take control of your event lifecycle with seamless registration, real-time tracking, and intelligent capacity management. Focus on creating great experiences while we handle the details.</p>
+</main>
 </div>
 
 <?php require_once 'includes/footer.php' ?>
