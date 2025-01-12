19394faef0c507a2af707712bd991a5a824ccccc
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-01-12 14:24:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+TODO
+
+- [ ] Add home page
+- [ ] Add register page
+- [ ] Add login page
+- [ ] Add event page
+- [ ] Add event create page
+- [ ] Add event edit page
+- [ ] Add event delete page
+- [ ] Add event view page
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php require_once 'includes/header.php' ?>
+
+<header class="p-8">
+    <h1 class="text-4xl font-bold">Eventer</h1>
+    <div><a href="register.php"></a></div>
+</header>
+
+<?php require_once 'includes/footer.php' ?>
