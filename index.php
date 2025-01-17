31ddeef70dc4651719c583888ef1bf00a38a36fb
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-01-17 21:19:41 +0800

    Index now redirects if already logged in

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php require_once 'includes/bootstrap.php' ?>
-<?php require_once 'includes/nav-guest.php' ?>
+<?php
+require_once 'includes/bootstrap.php';
+
+// If user is logged in, redirect to events page
+if (isset($_SESSION['user_id'])) {
+    header('Location: events.php');
+    exit();
+}
+
+// Log page visit
+logger()->info('Home page visited', [
+    'ip' => $_SERVER['REMOTE_ADDR'],
+    'userAgent' => $_SERVER['HTTP_USER_AGENT']
+]);
+
+require_once 'includes/nav-guest.php';
+?>
 
 <main class="text-center h-full flex-1 flex flex-col gap-8 items-center">
     <h2 class="text-7xl font-bold mt-72">Your events,<br> <span class="text-red-600">expertly managed</span></h2>
